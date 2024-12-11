0d6aa0bb8f0635f304e01d4d8556b8cfd4d5ddfd
seri-kie <serikawa.ed@gmail.com>
2024-12-11 11:26:18 +0900

    feat:ListTileのデザイン
    ListTileのデザインを作成

--- lib/pages/event_page/widgets/list_tile_normal.dart
+++ lib/pages/event_page/widgets/list_tile_normal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:warikan/models/event_normal.dart';
 
 class ListTileNormal extends StatelessWidget {
@@ -7,10 +8,42 @@ class ListTileNormal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(event.eventName),
-      subtitle:
-          Text('割り勘結果: ${event.remainPerPerson}円, 人数: ${event.remainPeople}人'),
+    // 日付のフォーマットを定義
+    final dateFormatter = DateFormat('yyyy/MM/dd');
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.all(10),
+      child: ListTile(
+        title: Text(event.eventName,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        subtitle: Text(event.remainPerPerson % 1 == 0 // 整数の場合
+            ? '1人:${event.remainPerPerson.toStringAsFixed(0)}円,人数: ${event.remainPeople}人' // 整数として表示
+            : '1人:${event.remainPerPerson.toStringAsFixed(3)}円,人数: ${event.remainPeople}人'),
+        leading: const CircleAvatar(
+          backgroundColor: Colors.lightGreen,
+          child: Icon(
+            Icons.calculate,
+            color: Colors.white,
+          ),
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              dateFormatter.format(event.date),
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Colors.grey[700],
+              ),
+            ),
+          ],
+        ),
+        onTap: () {
+          // イベント詳細ページへと遷移
+        },
+      ),
     );
   }
 }
